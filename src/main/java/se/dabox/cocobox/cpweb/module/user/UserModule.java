439ae901cabc3e7b8829adcfe661ba43abd78834
Jerker Klang <jerker.klang@learnifier.com>
2014-07-21 08:43:26 +0000

    Create a new role page
    git-svn-id: https://square.dwsbox.com/svn/dws/cpweb/branches/trainer-portal@1935 e88eb70a-f57a-4e2b-9716-5226b8acd346

--- src/main/java/se/dabox/cocobox/cpweb/module/user/UserModule.java
+++ src/main/java/se/dabox/cocobox/cpweb/module/user/UserModule.java
@@ -4,6 +4,10 @@
  */
 package se.dabox.cocobox.cpweb.module.user;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import net.unixdeveloper.druwa.RequestCycle;
@@ -12,17 +16,18 @@ import net.unixdeveloper.druwa.annotation.DefaultWebAction;
 import net.unixdeveloper.druwa.annotation.WebAction;
 import net.unixdeveloper.druwa.annotation.mount.WebModuleMountpoint;
 import net.unixdeveloper.druwa.freemarker.FreemarkerRequestTarget;
+import org.apache.commons.lang3.builder.CompareToBuilder;
 import se.dabox.cocobox.cpweb.module.core.AbstractWebAuthModule;
 import se.dabox.cocosite.druwa.CocoSiteConstants;
+import se.dabox.cocosite.infocache.InfoCacheHelper;
 import se.dabox.cocosite.login.CocositeUserHelper;
 import se.dabox.cocosite.org.MiniOrgInfo;
+import se.dabox.cocosite.security.role.CocoboxRoleUtil;
 import se.dabox.cocosite.user.MiniUserAccountHelper;
 import se.dabox.service.client.CacheClients;
 import se.dabox.service.common.ccbc.org.OrgRoleName;
 import se.dabox.service.login.client.UserAccount;
 import se.dabox.service.login.client.UserAccountService;
-import se.dabox.service.webutils.login.LoginUserAccountHelper;
-import se.dabox.util.collections.ValueUtils;
 
 /**
  *
@@ -57,7 +62,68 @@ public class UserModule extends AbstractWebAuthModule {
         return new FreemarkerRequestTarget("/user/userOverview.html", map);
     }
 
+    @WebAction
+    public RequestTarget onRoles(RequestCycle cycle, String strUserId) {
+        UserAccount user = getUserAccountService(cycle).getUserAccount(Long.valueOf(strUserId));
+
+        Locale userLocale = CocositeUserHelper.getUserLocale(cycle);
+
+        List<RoleInfo> roles = getRoles(cycle);
+
+        Map<String, Object> map = createMap();
+
+        map.put("user", user);
+        map.put("locale", userLocale);
+        map.put("roles", roles);
+
+        map.put("userimg", InfoCacheHelper.getInstance(cycle).getMiniUserInfo(user.getUserId()).
+                getThumbnail());
+
+        return new FreemarkerRequestTarget("/user/userRoles.html", map);
+    }
+
     private UserAccountService getUserAccountService(RequestCycle cycle) {
         return CacheClients.getClient(cycle, UserAccountService.class);
+    }
+
+    private List<RoleInfo> getRoles(RequestCycle cycle) {
+        Map<String, String> roleMap = new CocoboxRoleUtil().getCpRoles(cycle);
+
+        List<RoleInfo> infoList = new ArrayList<>();
+        for (Map.Entry<String, String> entry : roleMap.entrySet()) {
+            infoList.add(new RoleInfo(entry.getKey(), entry.getValue()));
+        }
+
+        Collections.sort(infoList, new Comparator<RoleInfo>() {
+
+            @Override
+            public int compare(RoleInfo o1, RoleInfo o2) {
+                return new CompareToBuilder().
+                        append(o1.getName(), o2.getName()).
+                        append(o1.getUuid(), o2.getUuid()).
+                        build();
+            }
+        });
+        
+        return infoList;
+    }
+
+    public static class RoleInfo {
+        private final String uuid;
+        private final String name;
+
+        public RoleInfo(String uuid, String name) {
+            this.uuid = uuid;
+            this.name = name;
+        }
+
+        public String getUuid() {
+            return uuid;
+        }
+
+        public String getName() {
+            return name;
+        }
+        
     }
 }
