cc2c4beaab1fa7c57560995535ff4824285e7da6
Jerker Klang <jerker.klang@learnifier.com>
2014-07-22 11:19:38 +0000

    Tweaked search result to contain back office users too
    git-svn-id: https://square.dwsbox.com/svn/dws/cpweb/branches/trainer-portal@1967 e88eb70a-f57a-4e2b-9716-5226b8acd346

--- src/main/java/se/dabox/cocobox/cpweb/module/project/role/ProjectRoleJsonModule.java
+++ src/main/java/se/dabox/cocobox/cpweb/module/project/role/ProjectRoleJsonModule.java
@@ -5,15 +5,21 @@ package se.dabox.cocobox.cpweb.module.project.role;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import net.unixdeveloper.druwa.RequestCycle;
 import net.unixdeveloper.druwa.RequestTarget;
 import net.unixdeveloper.druwa.annotation.WebAction;
 import net.unixdeveloper.druwa.annotation.mount.WebModuleMountpoint;
+import org.apache.commons.lang3.builder.CompareToBuilder;
 import se.dabox.cocobox.cpweb.module.core.AbstractJsonAuthModule;
 import se.dabox.cocosite.druwa.CocoSiteConstants;
 import se.dabox.cocosite.org.MiniOrgInfo;
 import se.dabox.cocosite.security.CocoboxSecurityConstants;
+import se.dabox.cocosite.security.UserAccountRoleCheck;
 import se.dabox.service.client.CacheClients;
 import se.dabox.service.common.ccbc.org.OrgRoleName;
 import se.dabox.service.login.client.UserAccount;
@@ -27,6 +33,7 @@ import se.dabox.service.webutils.login.LoginUserAccountHelper;
  */
 @WebModuleMountpoint("/project.rolejson")
 public class ProjectRoleJsonModule extends AbstractJsonAuthModule {
+    private static final int MAX_RESULT = 20;
 
     @WebAction
     public RequestTarget onSearchUser(RequestCycle cycle, String strOrgId) {
@@ -37,13 +44,24 @@ public class ProjectRoleJsonModule extends AbstractJsonAuthModule {
         //TOOD: Handle too short answers
         SearchContext ctx = new SearchContext(cycle, query, miniOrg);
 
-        List<UserAccount> matching = getMatching(ctx);
+        List<UserAccount> matching = getJsonUsers(ctx);
 
         return toJson(ctx, matching);
     }
 
+    private List<UserAccount> getJsonUsers(SearchContext ctx) {
+        List<UserAccount> users = getSortedMatching(ctx);
+
+        return users.subList(0, Math.min(users.size(), MAX_RESULT));
+    }
+
     private List<UserAccount> getMatching(SearchContext ctx) {
-        return findClientMatching(ctx);
+        List<UserAccount> all = new ArrayList<>();
+
+        all.addAll(findClientMatching(ctx));
+        all.addAll(findBoMatching(ctx));
+
+        return all;
     }
 
     private List<UserAccount> findClientMatching(SearchContext ctx) {
@@ -51,7 +69,7 @@ public class ProjectRoleJsonModule extends AbstractJsonAuthModule {
         final String orgRole = OrgRoleName.forOrg(ctx.org.getId()).toString();
 
         List<UserAccount> uas = getUserAccountService(ctx).searchUserAccounts(userId, ctx.term,
-                CocoSiteConstants.UA_PROFILE, orgRole, CocoboxSecurityConstants.USER_ROLE, 0, 20);
+                CocoSiteConstants.UA_PROFILE, orgRole, CocoboxSecurityConstants.USER_ROLE, 0, MAX_RESULT);
 
         return uas;
     }
@@ -87,6 +105,39 @@ public class ProjectRoleJsonModule extends AbstractJsonAuthModule {
         ByteArrayOutputStream json = dataTablesJson.encodeToStream(users);
 
         return jsonTarget(json);
+    }
+
+    private Collection<? extends UserAccount> findBoMatching(SearchContext ctx) {
+        UserAccount account = LoginUserAccountHelper.getUserAccount(ctx.cycle);
+
+        if (!UserAccountRoleCheck.isBoAdmin(account)) {
+            return Collections.emptyList();
+        }
+
+        List<UserAccount> uas = getUserAccountService(ctx).
+                searchUserAccounts(account.getUserId(), ctx.term,
+                null, null, CocoboxSecurityConstants.BOADMIN_ROLES, 0,
+                MAX_RESULT);
+
+        return uas;
+
+    }
+
+    private List<UserAccount> getSortedMatching(SearchContext ctx) {
+        List<UserAccount> users = getMatching(ctx);
+
+        Collections.sort(users, new Comparator<UserAccount>() {
+
+            @Override
+            public int compare(UserAccount o1, UserAccount o2) {
+                return new CompareToBuilder().append(o1.getDisplayName(), o2.getDisplayName()).
+                        append(o1.getPrimaryEmail(), o2.getPrimaryEmail()).
+                        append(o1.getUserId(), o2.getUserId()).
+                        build();
+            }
+        });
+
+        return users;
     }
 
     private static class SearchContext {
