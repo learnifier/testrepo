6e51a3771c7730a10f6664b8cc340a8e02674e4d
Jerker Klang <jerker.klang@learnifier.com>
2014-07-22 13:40:19 +0000

    Added projectPermission namespace for cpweb and tpweb
    git-svn-id: https://square.dwsbox.com/svn/dws/cpweb/branches/trainer-portal@1975 e88eb70a-f57a-4e2b-9716-5226b8acd346

--- src/main/java/se/dabox/cocobox/cpweb/freemarker/CpwebModifyViewHandler.java
+++ src/main/java/se/dabox/cocobox/cpweb/freemarker/CpwebModifyViewHandler.java
@@ -5,7 +5,10 @@ package se.dabox.cocobox.cpweb.freemarker;
 
 import net.unixdeveloper.druwa.freemarker.ModifyViewDataHandler;
 import net.unixdeveloper.druwa.freemarker.ViewDataEvent;
+import se.dabox.cocosite.druwa.security.ProjectSecurityNamespaceFactory;
 import se.dabox.cocosite.infocache.InfoCacheHelper;
+import se.dabox.cocosite.security.project.ProjectPermissionCheck;
+import se.dabox.service.common.ccbc.project.OrgProject;
 
 /**
  *
@@ -22,6 +25,13 @@ public class CpwebModifyViewHandler implements ModifyViewDataHandler {
                 InfoCacheHelper.getInstance(event.getCycle());
 
         event.getMap().put(FREEMARKER_ATTRIBUTE_NAME_INFOHELPER, infoHelper);
+
+        OrgProject project = (OrgProject) event.getMap().get("project");
+        if (project != null) {
+            ProjectPermissionCheck check = ProjectPermissionCheck.fromCycle(event.getCycle());
+            event.getMap().put("projectSecurity",
+                    new ProjectSecurityNamespaceFactory().createNamespace(project, check));
+        }
     }
 
 }
