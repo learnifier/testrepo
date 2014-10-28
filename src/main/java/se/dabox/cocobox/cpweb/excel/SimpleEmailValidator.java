508211185bdefa579ec9ceeac236369c66560e82
Jerker Klang <jerker.klang@learnifier.com>
2014-10-28 09:19:55 +0000

    Tweaked the email validator
    git-svn-id: https://square.dwsbox.com/svn/dws/cpweb/trunk@3206 e88eb70a-f57a-4e2b-9716-5226b8acd346

--- src/main/java/se/dabox/cocobox/cpweb/excel/SimpleEmailValidator.java
+++ src/main/java/se/dabox/cocobox/cpweb/excel/SimpleEmailValidator.java
@@ -4,6 +4,7 @@
 package se.dabox.cocobox.cpweb.excel;
 
 import java.util.regex.Pattern;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  *
@@ -22,9 +23,39 @@ public class SimpleEmailValidator implements EmailValidator {
         return INSTANCE;
     }
 
+    /**
+     * Performs a detailed check if the email is valid. A well formed email is not guaranteed to be
+     * valid. This method utilizes existing email validation libraries.
+     *
+     * @param string A String to check
+     *
+     * @return True if the string is a well formed email string.
+     */
+    public boolean isValidEmailDetailed(String string) {
+        return isValidEmail(string);
+    }
+
+    /**
+     * Performs a basic check if the email is well formed. A well formed email is not guaranteed to
+     * be valid. The following conditions are checked.
+     *
+     * <ul>
+     * <li>Not valid if the string contains , (comma) characters (although strictly speaking it's legal)</li>
+     * <li>Not valid if the string contains whitespace characters</li>
+     * <li>Not valid if the numbers of @ chars are not exactly 1</li>
+     * <li>Not valid if there are two consecutive . (dot) characters</li>
+     * </ul>
+     *
+     *
+     * @param string A String to check
+     * @return True if the string is a well formed email string.
+     */
     @Override
     public boolean isValidEmail(String string) {
-        return PATTERN.matcher(string).matches();
+        return  !StringUtils.contains(string, ',') &&
+                !StringUtils.containsWhitespace(string) &&
+                StringUtils.countMatches(string, "@") == 1 &&
+                StringUtils.countMatches(string, "..") == 0;
     }
 
 }
