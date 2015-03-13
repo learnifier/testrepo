a26753ff63c5b65d45ac6cf62afe4a33f2961792
Kevin Thulin <kevin.thulin@learnifier.com>
2015-03-13 15:06:00 +0000

    Started to implement bootstrap design for the listview
    git-svn-id: https://square.dwsbox.com/svn/dws/cpweb/branches/bootstrap@4760 e88eb70a-f57a-4e2b-9716-5226b8acd346

--- src/main/webapp/js/deeplink/listOrgMatDeeplinks.js
+++ src/main/webapp/js/deeplink/listOrgMatDeeplinks.js
@@ -1,45 +1,78 @@
-define(['knockout'],function(ko){
+define(['knockout', 'bootstrap/datepicker'], function (ko,datepicker) {
     "use strict";
-    
-    var DeepLinkModel = function(){
-      var self = this;
-      
-      self.materials = ko.observableArray();
+
+    var DeepLinkModel = function () {
+        var self = this;
+
+        self.materials = ko.observableArray();
     };
     
-    
-    var MaterialModel = function(){
-       var self = this;
+
+    var MaterialModel = function () {
+        var self = this;
+
+        self.id = ko.observable();
+        self.thumbnail = ko.observable();
+        self.title = ko.observable();
+        self.description = ko.observable();
+        self.status = ko.observable();
+        self.activeUntil = ko.observable();
+        self.url = ko.observable();
+        self.linkSectionVisible = ko.observable(false);
+
+        self.toggleLinkSection = function () {
+            if (self.linkSectionVisible() == true) {
+                self.linkSectionVisible(false);
+            } else {
+                $.post(listDeeplinksOrgMats.listOrgMatLinksUrl, {orgmatid: self.id()}, function (data) {
+                    self.activeUntil(data.aaData[0].activeto);
+                    self.url(data.aaData[0].deeplink);
+                    self.status(data.aaData[0].active);
+
+                    self.linkSectionVisible(true);
+                }).fail(function () {
+                    alert('failed to post data');
+                });
+            }
+
+        };
        
-       self.id = ko.observable();
-       self.thumbnail = ko.observable();
-       self.title = ko.observable();
-       self.description = ko.observable();
-       self.status = ko.observable();
-       self.activeUntil = ko.observable();
-       self.url = ko.observable();
+        self.changeActiveUntil = function(){
+            alert(self.activeUntil());
+        };
     };
     
-    $.get(listDeeplinksOrgMats.listOrgMatsUrl,function(data){
+    
+        ko.bindingHandlers.datePicker = {
+        init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+            
+            $(element).datepicker({
+                
+            });
+        }
+    };
+    
+  
+    
+
+    $.get(listDeeplinksOrgMats.listOrgMatsUrl, function (data) {
         var rootModel = new DeepLinkModel();
-        
-        $.each(data.aaData, function(){
-           var mm = new MaterialModel();
-           
-           mm.id(this.materialId);
-           mm.thumbnail(this.thumbnail);
-           mm.title(this.title);
-           mm.description(this.desc);
-           mm.status(this.activeLinks > 0);
-           //mm.activeUntil(this.)
-           //mm.url(this.)
-           rootModel.materials.push(mm);
+
+        $.each(data.aaData, function () {
+            var mm = new MaterialModel();
+
+            mm.id(this.materialId);
+            mm.thumbnail(this.thumbnail);
+            mm.title(this.title);
+            mm.description(this.desc);
+            mm.status(this.activeLinks > 0);
+            rootModel.materials.push(mm);
         });
-        
+
         ko.applyBindings(rootModel);
-        
-    }).error(function(){
+
+    }).fail(function () {
         alert('failed to load data');
     });
-    
+
 });
