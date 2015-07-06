ae1bd8aeee8bb672e45ea815978953a1738e8ac2
Magnus Andersson <magnus.andersson@learnifier.com>
2015-07-06 07:35:38 +0000

    Started work with  cug admin interface.
    git-svn-id: https://square.dwsbox.com/svn/dws/cpweb/branches/cug@6008 e88eb70a-f57a-4e2b-9716-5226b8acd346

--- src/main/webapp/js/cug/listClientUserGroups.js
+++ src/main/webapp/js/cug/listClientUserGroups.js
@@ -9,38 +9,13 @@ define([], function() {
         require(['dataTables-bootstrap', 'dataTables-responsive'], function() {
         var oTable = $('#listusers').dataTable({
             "dom": '<"row"<"col-sm-6"f><"col-sm-6">><"row"<"col-sm-12"rt>><"row"<"col-sm-6"i><"col-sm-6"p>>',
-            "order": [[1,'asc']],
+            "order": [[0,'asc']],
             "initComplete": function() {
                 $('#listusers_filter input').attr('placeholder', 'Search Client User Groups');
             },
             "columnDefs": [ 
                 {
                     "targets": [ 0 ],
-                    "orderable": false,
-                    "width": "32px",
-                    "data" : function(row, type, set) {
-                        if (!row.imagelink24Display) {
-                            if (row.imagelink24) {
-                                row.imagelink24Display = '<a href="'+ row.link + '"><img src="'+row.imagelink24+'" class="userimage" /></a> ';
-                            } else {
-                                row.imagelink24Display = '<a href="'+ row.link + '"><img src="${cocoboxCdn}/cocobox/img/cp/userImage_24x24.png" class="userimage" /></a> ';
-                            }
-                        }
-
-                        if (type === 'display') {
-                            return row.imagelink24Display;
-                        } else if (type === 'filter') {
-                            return row.imagelink24Display;
-                        } else if (type === 'sort') {
-                            return row.imagelink24Display;
-                        } else {
-                            //Anything else and raw row
-                            return row.imagelink24Display;
-                        }
-                    }
-                },
-                {
-                    "targets": [ 1 ],
                     "className": "block-link",
                     "data" : function(row, type, set) {
                         if (!row.nameDisplay) {
