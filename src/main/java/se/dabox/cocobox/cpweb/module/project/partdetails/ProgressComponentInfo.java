3513d81724f825253c5b00b0eeb91a5243997baa
Jerker Klang <jerker.klang@learnifier.com>
2015-07-09 12:00:32 +0000

    Improved the progress model
    git-svn-id: https://square.dwsbox.com/svn/dws/cpweb/branches/bootstrap@6081 e88eb70a-f57a-4e2b-9716-5226b8acd346

--- src/main/java/se/dabox/cocobox/cpweb/module/project/partdetails/ProgressComponentInfo.java
+++ src/main/java/se/dabox/cocobox/cpweb/module/project/partdetails/ProgressComponentInfo.java
@@ -3,8 +3,11 @@
  */
 package se.dabox.cocobox.cpweb.module.project.partdetails;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.UUID;
+import se.dabox.learnifier.cmi.CompletionStatus;
+import se.dabox.learnifier.cmi.SuccessStatus;
 import se.dabox.service.common.ccbc.material.OrgMaterial;
 import se.dabox.service.common.ccbc.project.TemporalProgressComponent;
 import se.dabox.service.common.coursedesign.v1.Component;
@@ -23,9 +26,13 @@ class ProgressComponentInfo {
     private Component component;
     private OrgMaterial orgMat;
     private Date completed;
+    private SuccessStatus successStatus;
+    private CompletionStatus completionStatus;
+    private BigDecimal score;
 
-    private ProgressComponentInfo(UUID cid, ProgressComponentType type, Product product,
-            Component component, OrgMaterial orgMat, Date completed) {
+    public ProgressComponentInfo(UUID cid, ProgressComponentType type, Product product,
+            Component component, OrgMaterial orgMat, Date completed, SuccessStatus successStatus,
+            CompletionStatus completionStatus, BigDecimal score) {
         ParamUtil.required(cid,"cid");
         ParamUtil.required(type,"type");
         this.cid = cid;
@@ -34,6 +41,14 @@ class ProgressComponentInfo {
         this.component = component;
         this.orgMat = orgMat;
         this.completed = completed;
+        this.successStatus = successStatus;
+        this.completionStatus = completionStatus;
+        this.score = score;
+    }
+
+    private ProgressComponentInfo(UUID cid, ProgressComponentType type, Product product,
+            Component component, OrgMaterial orgMat, Date completed) {
+        this(cid, type, product, component, orgMat, completed, null, null, null);
     }
 
     static ProgressComponentInfo forProduct(UUID cid, ProgressComponentType type, Product product) {
@@ -89,12 +104,37 @@ class ProgressComponentInfo {
     public void setOrgMat(OrgMaterial orgMat) {
         this.orgMat = orgMat;
     }
-    
+
+    public SuccessStatus getSuccessStatus() {
+        return successStatus;
+    }
+
+    public CompletionStatus getCompletionStatus() {
+        return completionStatus;
+    }
+
+    public BigDecimal getScore() {
+        return score;
+    }
+
+    public void setSuccessStatus(SuccessStatus successStatus) {
+        this.successStatus = successStatus;
+    }
+
+    public void setCompletionStatus(CompletionStatus completionStatus) {
+        this.completionStatus = completionStatus;
+    }
+
+    public void setScore(BigDecimal score) {
+        this.score = score;
+    }
+
     @Override
     public String toString() {
         return "ProgressComponentInfo{" + "cid=" + cid + ", type=" + type + ", product=" + product +
-                ", component=" + component + ", orgMat=" + orgMat + ", completed=" + completed + '}';
+                ", component=" + component + ", orgMat=" + orgMat + ", completed=" + completed +
+                ", successStatus=" + successStatus + ", completionStatus=" + completionStatus +
+                ", score=" + score + '}';
     }
 
-    
 }
