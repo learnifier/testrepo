f83cef051c8d238d9c9ceb2f960b12081c013eda
Jerker Klang <jerker.klang@learnifier.com>
2015-08-25 13:35:32 +0000

    Fixed better goto project logic that are 'preview on userweb' aware
    git-svn-id: https://square.dwsbox.com/svn/dws/cpweb/trunk@6344 e88eb70a-f57a-4e2b-9716-5226b8acd346

--- src/main/java/se/dabox/cocobox/cpweb/module/CpGotoModule.java
+++ src/main/java/se/dabox/cocobox/cpweb/module/CpGotoModule.java
@@ -5,10 +5,24 @@ package se.dabox.cocobox.cpweb.module;
 
 import net.unixdeveloper.druwa.RequestCycle;
 import net.unixdeveloper.druwa.RequestTarget;
+import net.unixdeveloper.druwa.annotation.AroundInvoke;
 import net.unixdeveloper.druwa.annotation.WebAction;
 import net.unixdeveloper.druwa.annotation.mount.WebModuleMountpoint;
+import net.unixdeveloper.druwa.module.InvocationContext;
+import net.unixdeveloper.druwa.request.RedirectUrlRequestTarget;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import se.dabox.cocobox.cpweb.NavigationUtil;
 import se.dabox.cocobox.cpweb.module.core.AbstractModule;
+import se.dabox.cocosite.druwa.CocoSiteConfKey;
+import se.dabox.cocosite.security.CocoboxPermissions;
+import se.dabox.cocosite.security.CocositeLoginChecker;
+import se.dabox.cocosite.security.project.ProjectPermissionCheck;
+import se.dabox.service.common.ccbc.project.OrgProject;
+import se.dabox.service.common.context.DwsRealmHelper;
+import se.dabox.service.login.client.UserAccount;
+import se.dabox.service.webutils.login.LoginUserAccountHelper;
+import se.dabox.service.webutils.login.nlogin.AbstractNewLoginChecker;
 
 /**
  * Module used for various goto operations. The gotos are high level actions that this module can
@@ -18,10 +32,59 @@ import se.dabox.cocobox.cpweb.module.core.AbstractModule;
  */
 @WebModuleMountpoint("/goto")
 public class CpGotoModule extends AbstractModule {
-    //Do not extend from an auth class. We wan't to be able to run this without security
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(CpGotoModule.class);
 
+    private final AbstractNewLoginChecker loginChecker = new CocositeLoginChecker();
+
+    @AroundInvoke(order = 500)
+    public final Object loginCheck(InvocationContext ctx) {
+        return loginChecker.loginCheck(ctx);
+    }
+
+    //Do not extend from an auth class. We want to be able to run this without security
     @WebAction
     public RequestTarget onProject(RequestCycle cycle, String strProjectId) {
+        UserAccount user = LoginUserAccountHelper.getUserAccount(cycle);
+
+        OrgProject project
+                = getCocoboxCordinatorClient(cycle).getProject(Long.valueOf(strProjectId));
+
+        if (project == null) {
+            LOGGER.debug("Project {} doesn't exist. Redirecting to boweb");
+
+             String boweb = DwsRealmHelper.getRealmConfiguration(cycle).getValue(
+                    CocoSiteConfKey.BOWEB_BASEURL);
+
+            return new RedirectUrlRequestTarget(boweb);
+        }
+
+        ProjectPermissionCheck ppc = ProjectPermissionCheck.fromCycle(cycle);
+
+        if (ppc.checkPermission(project, CocoboxPermissions.CP_ACCESS) &&
+                ppc.checkPermission(project, CocoboxPermissions.CP_VIEW_PROJECT)) {
+
+            LOGGER.debug("Redirecting to client portal page for project {}", strProjectId);
+
+            NavigationUtil.toProjectPage(Long.valueOf(strProjectId));
+        } else if (ppc.checkPermission(project, CocoboxPermissions.PRJ_PREVIEW_ON_USERPORTAL)) {
+            LOGGER.debug("Redirecting to user portal to active preview for project {}", strProjectId);
+
+            String upweb = DwsRealmHelper.getRealmConfiguration(cycle).getValue(
+                    CocoSiteConfKey.UPWEB_BASEURL);
+
+            String url = upweb+"user/preview/"+strProjectId;
+
+            return new RedirectUrlRequestTarget(url);
+        } else {
+            //LOGGER.warn("Unable to determine where to redirect user {} for project {}. Sending user to userweb.", projectPerms.get)
+
+            String upweb = DwsRealmHelper.getRealmConfiguration(cycle).getValue(
+                    CocoSiteConfKey.UPWEB_BASEURL);
+
+            return new RedirectUrlRequestTarget(upweb);
+        }
+
         return NavigationUtil.toProjectPage(Long.valueOf(strProjectId));
     }
 }
