07a0f7aa4153d3805e36065d9080dba7d1a84c1d
Jerker Klang <jerker.klang@learnifier.com>
2015-09-16 05:42:26 +0000

    fixed: npe when creating projects with crisp products that doesn't have getProjectConfig
    git-svn-id: https://square.dwsbox.com/svn/dws/cpweb/trunk@6777 e88eb70a-f57a-4e2b-9716-5226b8acd346

--- src/main/java/se/dabox/cocobox/cpweb/module/project/CreateProjectSessionProcessor.java
+++ src/main/java/se/dabox/cocobox/cpweb/module/project/CreateProjectSessionProcessor.java
@@ -338,7 +338,7 @@ public class CreateProjectSessionProcessor implements NewProjectSessionProcessor
                         cycle, state));
             }
 
-            if (response.isEmpty()) {
+            if (response == null || response.isEmpty()) {
                 continue;
             }
 
