395735da051de2da328de521e38176b3520cfe69
Magnus Andersson <magnus.andersson@learnifier.com>
2016-03-04 13:45:53 +0100

    Experimenting with simple file/folder structure.

--- src/main/webapp/js/material/listMaterialsFolder.js
+++ src/main/webapp/js/material/listMaterialsFolder.js
@@ -8,37 +8,80 @@ define(['knockout'], function (ko) {
 
     var exports = {};
 
+    var Item = function(id, name, typeTitle, thumbnail) {
+        var self = {};
+        self.id = id;
+        self.name = name;
+        self.typeTitle = typeTitle;
+        self.thumbnail = thumbnail;
+        return self;
+    }
+
+    var Folder = function (id, name, folders) {
+        var self = Item(id, name, "", "Folder");
+        self.folders = folders;
+        self.materials = [];
+        
+        return self;
+    }
+
+    function Material(id, name, typeTitle, thumbnail) {
+        var self = Item(id, name, typeTitle, thumbnail);
+        self.name = name;
+        self.typeTitle = typeTitle;
+        self.thumbnail = thumbnail;
+    }
+
+
+    function parseFolders(json) {
+        var folderHash = {};
+
+        function parseFoldersInner(fs) {
+            return $.map(fs, function(f){
+                var nf = new Folder(f.id, f.name, parseFoldersInner(f.folders));
+                folderHash[f.id] = nf;
+                return nf;
+            });
+        }
+        var nf = new Folder(1377, "/", parseFoldersInner(json));
+        folderHash[1377] = nf;
+        return {
+            folders: nf,
+            folderHash: folderHash
+        };
+    }
+
+
     function ListMaterialModel() {
         var self = this;
-
-        function Row(name, typeTitle, thumbnail) {
-            var self = this;
-            self.name = name;
-            self.typeTitle = typeTitle;
-            self.thumbnail = thumbnail;
-        }
 
         self.rows = ko.observableArray();
 
         self.products = undefined;
 
+        self.folders = undefined;
+        self.folderHash = undefined;
+
         self.readAjax = function(url) {
-            console.log("Calling ", url);
 
             $.getJSON(url).done(function(data){
                 console.log("Got data: ", data);
 
-                //parseFolders();
-
+                var folderInfo = parseFolders(data.folders);
+                self.folderHash = folderInfo.folderHash;
+                self.folders = folderInfo.folders;
                 var res = $.map(data.aaData, function(item) {
-                   return new Row(item.title, item.typeTitle, item.thumbnail);
+                    var r = new Material(item.id, item.title, item.typeTitle, item.thumbnail);
+                    var materialFolderId  = item.materialFolderId;
+                    if(materialFolderId === null || materialFolderId === undefined) {
+                        materialFolderId = 1377;
+                    }
+                    console.log("Adding", materialFolderId, r);
+                    self.folderHash[materialFolderId].materials.push(r);
+                    return r;
                 });
-
-                var folders = $.map(data.folders, function(item) {
-                    //item.id, item.folders, item.name
-                    return new Row(item.name, "Folder", item.thumbnail);
-                });
-                self.rows(res.concat(folders));
+                console.log("Setting rows: ", self.folderHash[1377].folders, self.folderHash[1377].material);
+                self.rows(self.folderHash[1377].folders.concat(self.folderHash[1377].materials));
             });
 
         }
