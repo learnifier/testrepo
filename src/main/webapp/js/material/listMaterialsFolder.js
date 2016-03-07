1f0b23ef719309f0538f66fb46f4c612424450f9
Magnus Andersson <magnus.andersson@learnifier.com>
2016-03-07 14:34:37 +0100

    Refactored a bit.

--- src/main/webapp/js/material/listMaterialsFolder.js
+++ src/main/webapp/js/material/listMaterialsFolder.js
@@ -7,102 +7,93 @@ define("cocobox-list", ['knockout', 'dabox-common', 'messenger'], function (ko) 
     "use strict";
 
     var exports = {};
-
-    var model;
-
-    var Item = function(id, parentId, name, typeTitle, thumbnail) {
-        var self = this;
-        self.id = id;
-        self.parentId = parentId;
-        self.name = name;
-        self.typeTitle = typeTitle;
-        self.thumbnail = thumbnail;
-        self.selected = ko.observable(false);
-        self.selectRow = function(){
-            var s = self.selected();
-            model.updateSelected(self, !s);
-            self.selected(!s);
-        };
-    };
-
-    var Folder = function (id, parentId, name, folders) {
-        var self = this;
-        Item.call(this, id, parentId, name, "Folder", "");
-        self.folders = ko.observableArray(folders);
-        self.materials = ko.observableArray();
-        self.clickName = function(e) {
-            if(model) {
-                model.showFolder(id);
-            }
-        }
-
-        self.removeChild = function(child){
-            var a;
-            if(child instanceof Folder) {
-                a = model.selectedFolder().folders;
-            } else {
-                a = model.selectedFolder().materials;
-            }
-            var index = a.indexOf(child);
-            if (index > -1) {
-                a.splice(index, 1);
-                return true;
-            }
-            return false;
-        };
-    };
-
-    function Material(id, parentId, name, typeTitle, thumbnail) {
-        var self = this;
-        Item.call(this, id, parentId, name, typeTitle, thumbnail);
-        self.name = name;
-        self.typeTitle = typeTitle;
-
-        self.clickName = function() {
-            cocobox.infoDialog("Preview", "Nice material preview here.", function(){});
-        }
-    };
-
-
-    function parseFolders(json) {
-        var folderHash = {};
-
-        function parseFoldersInner(fs, parentId) {
-            return $.map(fs, function(f){
-                var nf = new Folder(f.id, parentId, f.name, parseFoldersInner(f.folders, f.id));
-                folderHash[f.id] = nf;
-                return nf;
-            });
-        }
-        var nf = new Folder(1337, undefined, "/", parseFoldersInner(json, 1337));
-        folderHash[1337] = nf;
-        return {
-            folders: nf,
-            folderHash: folderHash
-        };
-    }
-
-
     function ListModel(params) {
-        model = this; // TODO: Fix this
-        var self = this;
-        this.name = params.name + "!";
+        var model = this, self = this;
+
+        // ----------------------------------
+        var Item = function(id, parentId, name, typeTitle, thumbnail) {
+            var self = this;
+            self.id = id;
+            self.parentId = parentId;
+            self.name = name;
+            self.typeTitle = typeTitle;
+            self.thumbnail = thumbnail;
+            self.selected = ko.observable(false);
+            self.selectRow = function(){
+                var s = self.selected();
+                model.updateSelected(self, !s);
+                self.selected(!s);
+            };
+        };
+
+        var Folder = function (id, parentId, name, folders) {
+            var self = this;
+            Item.call(this, id, parentId, name, "Folder", "");
+            self.folders = ko.observableArray(folders);
+            self.materials = ko.observableArray();
+            self.clickName = function(e) {
+                if(model) {
+                    model.showFolder(id);
+                }
+            }
+
+            self.removeChild = function(child){
+                var a;
+                if(child instanceof Folder) {
+                    a = model.selectedFolder().folders;
+                } else {
+                    a = model.selectedFolder().materials;
+                }
+                var index = a.indexOf(child);
+                if (index > -1) {
+                    a.splice(index, 1);
+                    return true;
+                }
+                return false;
+            };
+        };
+
+        function Material(id, parentId, name, typeTitle, thumbnail) {
+            var self = this;
+            Item.call(this, id, parentId, name, typeTitle, thumbnail);
+            self.name = name;
+            self.typeTitle = typeTitle;
+
+            self.clickName = function() {
+                cocobox.infoDialog("Preview", "Nice material preview here.", function(){});
+            }
+        };
+
+
+        function parseFolders(json) {
+            var folderHash = {};
+
+            function parseFoldersInner(fs, parentId) {
+                return $.map(fs, function(f){
+                    var nf = new Folder(f.id, parentId, f.name, parseFoldersInner(f.folders, f.id));
+                    folderHash[f.id] = nf;
+                    return nf;
+                });
+            }
+            var nf = new Folder(1337, undefined, "/", parseFoldersInner(json, 1337));
+            folderHash[1337] = nf;
+            return {
+                folders: nf,
+                folderHash: folderHash
+            };
+        }
+        // ----------------------------------
 
         self.selectedFolder = ko.observable();
-        //self.rows = ko.observableArray();
         self.rows = function(){
-            console.log("Rows: ", self, self.selectedFolder());
             if(self.selectedFolder()) {
                 return self.selectedFolder().folders().concat(self.selectedFolder().materials());
             } else {
                 return [];
             }
         };
-        self.products = undefined;
-
         self.folders = undefined;
         self.folderHash = undefined;
-
         self.selected = ko.observableArray();
 
 
@@ -151,7 +142,6 @@ define("cocobox-list", ['knockout', 'dabox-common', 'messenger'], function (ko) 
                             self.selectedFolder().removeChild(r.item);
                         }
                     });
-
                     if(okCount>0) {
                         CCBMessengerInfo("Removed " + okCount  + " item(s)");
                     }
@@ -171,17 +161,14 @@ define("cocobox-list", ['knockout', 'dabox-common', 'messenger'], function (ko) 
         };
 
         self.showFolder = function(folderId) {
-            console.log("showFolder", folderId);
             self.selectedFolder(self.folderHash[folderId]);
         };
 
         params.getData().done(function(data){
-            console.log("rows: ", data.rows);
-            console.log("folders: ", data.folders);
             var folderInfo = parseFolders(data.folders);
             self.folderHash = folderInfo.folderHash;
             self.folders = folderInfo.folders;
-            var res = $.map(data.rows, function(item) {
+            $.each(data.rows, function(i, item) {
                 var materialFolderId  = item.materialFolderId;
                 var r = new Material(item.id, materialFolderId, item.title, item.typeTitle, item.thumbnail);
                 if(materialFolderId === null || materialFolderId === undefined) {
@@ -237,19 +224,26 @@ define(['knockout', 'dabox-common', 'cocobox-list'], function (ko) {
                     }
                 }],
                 moveFn: function(items, toFolder) {
-                    return $.map(items, function(item){
-                        return ({ status: "ok", item: item});
-                    });
+                    var deferred = $.Deferred();
+                    window.setTimeout(function(){
+                        deferred.resolve($.map(items, function(item){
+                            return ({ status: "ok", item: item });
+                        }));
+                    }, 500);
+                    return deferred.promise();
                 },
                 copyFn: function(items, toFolder) {
-                    return $.map(items, function(item){
-                        return ({ status: "ok", item: item});
-                    });
+                    var deferred = $.Deferred();
+                    window.setTimeout(function(){
+                        deferred.resolve($.map(items, function(item){
+                            return ({ status: "ok", item: item });
+                        }));
+                    }, 500);
+                    return deferred.promise();
                 },
                 removeFn: function(items) {
                     var deferred = $.Deferred();
                     window.setTimeout(function(){
-                        console.log("Resolving...", items);
                         deferred.resolve($.map(items, function(item){
                             return ({ status: "ok", item: item });
                         }));
