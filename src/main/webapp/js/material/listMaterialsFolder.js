9858c5f663a7a230d0d78807f39d3361b86eb95d
Magnus Andersson <magnus.andersson@learnifier.com>
2016-03-24 12:48:17 +0100

    Removed comment.

--- src/main/webapp/js/material/listMaterialsFolder.js
+++ src/main/webapp/js/material/listMaterialsFolder.js
@@ -124,7 +124,6 @@ define(['knockout', 'cocobox/ccb-imodal', 'dabox-common', 'cocobox/ko-components
         function openCreateMaterial(url, type) {
             var folderId = self.api.currentFolderId(),
                 url = url + "&type=" + type;
-            console.log("bool: ", folderId, folderId?"true":"false");
             if(folderId) { // Can be 0 which means false
                 url += "&folder=" + folderId;
             }
