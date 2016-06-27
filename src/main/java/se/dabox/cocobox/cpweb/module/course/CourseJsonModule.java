1dbbeb2ef4e82acbcd4f6d7db76bfd7eae343b12
Magnus Andersson <magnus.andersson@learnifier.com>
2016-06-27 10:59:09 +0200

    Started course admin page.

---
+++ src/main/java/se/dabox/cocobox/cpweb/module/course/CourseJsonModule.java
@@ -0,0 +1,92 @@
+/*
+ * (c) Dabox AB 2016 All Rights Reserved
+ */
+package se.dabox.cocobox.cpweb.module.course;
+
+import net.unixdeveloper.druwa.RequestCycle;
+import net.unixdeveloper.druwa.RequestTarget;
+import net.unixdeveloper.druwa.annotation.WebAction;
+import net.unixdeveloper.druwa.annotation.mount.WebModuleMountpoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import se.dabox.cocobox.cpweb.module.core.AbstractJsonAuthModule;
+import se.dabox.service.common.ccbc.CocoboxCoordinatorClient;
+import se.dabox.service.common.ccbc.project.OrgProject;
+import se.dabox.service.common.ccbc.project.OrgProjectPredicates;
+import se.dabox.service.common.ccbc.project.ProjectSubtypeConstants;
+import se.dabox.service.webutils.login.LoginUserAccountHelper;
+import se.dabox.util.collections.CollectionsUtil;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ *
+ * @author Magnus Andersson (magnus.andersson@learnifier.com)
+ */
+
+@WebModuleMountpoint("/course.json")
+public class CourseJsonModule extends AbstractJsonAuthModule {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(CourseJsonModule.class);
+
+    @WebAction
+    public RequestTarget onListOrgCourses(RequestCycle cycle, String strOrgId)
+            throws Exception {
+        checkOrgPermission(cycle, strOrgId);
+        long orgId = Long.valueOf(strOrgId);
+
+        CocoboxCoordinatorClient ccbc = getCocoboxCordinatorClient(cycle);
+
+        long caller = LoginUserAccountHelper.getUserId(cycle);
+
+        List<OrgProject> projects =
+                ccbc.listOrgProjects(orgId);
+
+//        ByteArrayOutputStream os = toJsonObjectProjects(cycle, projects);
+
+        return jsonTarget(Collections.singletonMap("courses", Collections.emptyMap()));
+    }
+
+    @WebAction
+    public RequestTarget onCourseInfo(RequestCycle cycle, String strCourseId)
+            throws Exception {
+//        checkPermission(cycle, strCourseId);
+
+//        return jsonTarget(Collections.singletonMap("members", uas.size()));
+        return jsonTarget(Collections.singletonMap("course", Collections.emptyMap()));
+    }
+
+
+
+//    protected void checkPermission(RequestCycle cycle, Course course, String courseId) {
+//        if (course == null) {
+//            LOGGER.warn("Project {} doesn't exist.", courseId);
+//
+//            ErrorCodeRequestTarget error
+//                    = new ErrorCodeRequestTarget(HttpServletResponse.SC_NOT_FOUND);
+//            throw new RetargetException(error);
+//        } else {
+//            super.checkPermission(cycle, course);
+//        }
+//    }
+
+//    private byte[] toJson(final List<UserAccount> uas) {
+//        return new JsonEncoding() {
+//            @Override
+//            protected void encodeData(JsonGenerator generator) throws IOException {
+//                generator.writeStartObject();
+//                generator.writeArrayFieldStart("aaData");
+//                for(UserAccount ua: uas) {
+//                    generator.writeStartObject();
+//                    generator.writeNumberField("userId", ua.getUserId());
+//                    generator.writeEndObject();
+//                }
+//                generator.writeEndArray();
+//                generator.writeEndObject();
+//            }
+//        }.encode();
+//    }
+}
---
+++ src/main/java/se/dabox/cocobox/cpweb/module/course/CourseModule.java
@@ -0,0 +1,58 @@
+/*
+ * (c) Dabox AB 2016 All Rights Reserved
+ */
+package se.dabox.cocobox.cpweb.module.course;
+
+import net.unixdeveloper.druwa.RequestCycle;
+import net.unixdeveloper.druwa.RequestTarget;
+import net.unixdeveloper.druwa.annotation.WebAction;
+import net.unixdeveloper.druwa.annotation.mount.WebModuleMountpoint;
+import net.unixdeveloper.druwa.freemarker.FreemarkerRequestTarget;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import se.dabox.cocobox.cpweb.module.project.AbstractProjectWebModule;
+import se.dabox.cocosite.org.MiniOrgInfo;
+
+import java.util.Map;
+
+/**
+ *
+ * @author Magnus Andersson (magnus.andersson@learnifier.com)
+ */
+
+@WebModuleMountpoint("/courses")
+public class CourseModule extends AbstractProjectWebModule {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(CourseModule.class);
+    public static final String LIST_ACTION = "list";
+    public static final String OVERVIEW_ACTION = "overview";
+
+    @WebAction
+    public RequestTarget onList(RequestCycle cycle, String strOrgId) {
+        MiniOrgInfo org = secureGetMiniOrg(cycle, strOrgId);
+
+        Map<String, Object> map = createMap();
+
+        map.put("org", org);
+
+        return new FreemarkerRequestTarget("/course/listCourses.html", map);
+    }
+
+    @WebAction
+    public RequestTarget onOverview(RequestCycle cycle, String courseId) {
+
+//        OrgProject project =
+//                getProject(cycle, courseId);
+//
+//        checkPermission(cycle, course);
+//        checkCoursePermission(cycle, course, CocoboxPermissions.CP_VIEW_PROJECT);
+
+        Map<String, Object> map = createMap();
+
+//        addCommonMapValues(map, project, cycle);
+
+        return new FreemarkerRequestTarget("/project/courseOverview.html", map);
+    }
+
+}
