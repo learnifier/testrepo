849a0bdcee2cef3a77ff942b12ac762694d966ea
Magnus Andersson <magnus.andersson@learnifier.com>
2016-07-01 16:53:20 +0200

    Now passing on courseId to new project.

--- src/main/webapp/js/course/listCourses.js
+++ src/main/webapp/js/course/listCourses.js
@@ -52,7 +52,7 @@ define(['cocobox/ccb-imodal', 'es6-shim'], function(ccbImodal) {
                                 .append($('<a />', {href: settings.sessionDetailsUrl + "/" + "1212"}) // TODO: Add project ID here once it is available
                                     .text(item.name))));
                 });
-                tbody.append($("<a />", {"class": "btn btn-primary", "href": settings.newSessionUrl + "?courseId=" + d.id.id}).text("Add Session"));
+                tbody.append($("<a />", {"class": "btn btn-primary", "href": settings.newSessionUrl + "/" + d.id.id}).text("Add Session"));
 
                 deferred.resolve(table);
             });
