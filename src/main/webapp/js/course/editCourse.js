4ccbdb5fdfe20019405a22afc4b8a7793969b8ef
Jerker Klang <jerker.klang@learnifier.com>
2016-07-05 15:04:54 +0200

    Added support for picking an image as well

--- src/main/webapp/js/course/editCourse.js
+++ src/main/webapp/js/course/editCourse.js
@@ -1,7 +1,7 @@
 /*
  * (c) Dabox AB 2016 All Rights Reserved
  */
-define(['knockout', 'cocobox/ccb-imodal', 'es6-shim', 'ckeditor4', 'cocobox-knockout-bindings', 'messenger'], function(ko, ccbImodal) {
+define(['knockout', 'cocobox/ccb-imodal', 'es6-shim', 'ckeditor4', 'cocobox-knockout-bindings', 'messenger', 'jquery.fileupload'], function(ko, ccbImodal) {
     "use strict";
     var exports = {}, settings, imodalClient;
 
@@ -20,6 +20,9 @@ define(['knockout', 'cocobox/ccb-imodal', 'es6-shim', 'ckeditor4', 'cocobox-knoc
         this.initializing = ko.observable(true);
         this.name = ko.observable();
         this.description = ko.observable();
+        this.crl = ko.observable();
+        this.progressPercent = ko.observable(0);
+        this.viewLink = ko.observable();
 
         if(settings.courseId) {
             $.getJSON(settings.getCourseUrl + "/" + settings.courseId).done(function(data){
@@ -65,7 +68,7 @@ define(['knockout', 'cocobox/ccb-imodal', 'es6-shim', 'ckeditor4', 'cocobox-knoc
         };
 
         this.validate = function(){
-            return this.name();
+            return this.name() && this.crl();
         };
     }
 
@@ -74,13 +77,43 @@ define(['knockout', 'cocobox/ccb-imodal', 'es6-shim', 'ckeditor4', 'cocobox-knoc
             getCourseUrl: undefined,
             saveCourseUrl: undefined,
             createCourseUrl: undefined,
-            courseId: undefined
+            courseId: undefined,
+            defaultImage: undefined
         }, options || {});
 
         console.log("");
         var courseModel = new CourseModel();
+        courseModel.viewLink(settings.defaultImage);
         ko.applyBindings(courseModel);
 
+        $("#fileupload").fileupload({
+            progress: function (e, data) {
+                console.log(data);
+                var progress = parseInt(data.loaded / data.total * 100, 10);
+                courseModel.progressPercent(progress);
+            },
+            fail: function (e, data) {
+                console.log("fail", data);
+                courseModel.progressPercent(0);
+                require(['dabox-common'], function() {
+                        cocobox.errorDialog("Upload failed", "Upload failed. Make sure that the file you selected is a valid image");
+                });
+                $("#uploadPbar").hide();
+            },
+            done: function (e, data) {
+                console.log("done", data);
+                $("#uploadPbar").hide();
+                if (data.result.status === 'ok') {
+                    courseModel.crl(data.result.crl);
+                    courseModel.viewLink(data.result.viewLink);
+                } else {
+                    require(['dabox-common'], function() {
+                        cocobox.errorDialog("Upload failed", "Upload failed. Make sure that the file you selected is a valid image");
+                    });
+                }
+                courseModel.progressPercent(0);
+            }
+        });
     };
 
 
