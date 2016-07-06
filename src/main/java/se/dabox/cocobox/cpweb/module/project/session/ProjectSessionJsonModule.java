a05caa4bc8cfcd3f1b6f1e4b2ee57e7e0192956b
Magnus Andersson <magnus.andersson@learnifier.com>
2016-07-06 17:01:47 +0200

    Enrollment/disenrollment.

--- src/main/java/se/dabox/cocobox/cpweb/module/project/session/ProjectSessionJsonModule.java
+++ src/main/java/se/dabox/cocobox/cpweb/module/project/session/ProjectSessionJsonModule.java
@@ -12,6 +12,8 @@ import org.slf4j.LoggerFactory;
 import se.dabox.cocobox.cpweb.module.core.AbstractJsonAuthModule;
 import se.dabox.service.coursecatalog.client.CourseCatalogClient;
 import se.dabox.service.coursecatalog.client.session.*;
+import se.dabox.service.coursecatalog.client.session.impl.StandardDisenrollmentSettings;
+import se.dabox.service.coursecatalog.client.session.impl.StandardEnrollmentSettings;
 import se.dabox.service.coursecatalog.client.session.impl.StandardSessionVisibility;
 import se.dabox.service.coursecatalog.client.session.list.ListCatalogSessionRequestBuilder;
 import se.dabox.service.coursecatalog.client.session.update.UpdateSessionRequest;
@@ -19,7 +21,10 @@ import se.dabox.service.coursecatalog.client.session.update.UpdateSessionRequest
 import se.dabox.service.webutils.login.LoginUserAccountHelper;
 import se.dabox.util.collections.CollectionsUtil;
 
+import java.time.Instant;
 import java.util.*;
+
+import static se.dabox.cocobox.cpweb.module.project.session.SessionField.visibility;
 
 /**
  *
@@ -30,16 +35,17 @@ public class ProjectSessionJsonModule extends AbstractJsonAuthModule {
 
     private static final Logger LOGGER =
             LoggerFactory.getLogger(ProjectSessionJsonModule.class);
-    
+
     @WebAction
     public RequestTarget onSetCourseSessionField(RequestCycle cycle, String strCourseSessionId) {
-        long caller = LoginUserAccountHelper.getUserId(cycle);
-        CatalogCourseSessionId courseSessionId = CatalogCourseSessionId.valueOf(Integer.valueOf(strCourseSessionId));
+        final long caller = LoginUserAccountHelper.getUserId(cycle);
+        final CatalogCourseSessionId courseSessionId = CatalogCourseSessionId.valueOf(Integer.valueOf(strCourseSessionId));
 
+        final String pk = cycle.getRequest().getParameter("pk");
+        final String value = cycle.getRequest().getParameter("value");
         final CourseCatalogClient ccc = getCourseCatalogClient(cycle);
-        String pk = cycle.getRequest().getParameter("pk");
-        String value = cycle.getRequest().getParameter("value");
         final CatalogCourseSession session = CollectionsUtil.singleItemOrNull(ccc.listSessions(new ListCatalogSessionRequestBuilder().withId(courseSessionId).build()));
+
         checkSessionPermission(cycle, session);
         final SessionField sessionField = SessionField.valueOf(pk);
         sessionField.accept(new SessionField.SessionFieldVisitor<Void>(){
@@ -52,16 +58,85 @@ public class ProjectSessionJsonModule extends AbstractJsonAuthModule {
 
             @Override
             public Void visitVisibility() {
-                VisibilityMode visibilityMode = VisibilityMode.valueOf(value);
+                VisibilityMode mode = VisibilityMode.valueOf(value);
                 final SessionVisibility visibility = session.getVisibility();
                 final StandardSessionVisibility newVisibility;
                 if(visibility == null) {
-                    newVisibility = new StandardSessionVisibility(null, null, visibilityMode);
+                    newVisibility = new StandardSessionVisibility(null, null, mode);
                 } else {
-                    newVisibility = new StandardSessionVisibility(visibility.getFrom(), visibility.getTo(), visibilityMode);
+                    newVisibility = new StandardSessionVisibility(visibility.getFrom(), visibility.getTo(), mode);
                 }
                 final UpdateSessionRequest usr = UpdateSessionRequestBuilder.newBuilder(caller, courseSessionId).setVisibilitySettings(newVisibility).createUpdateSessionRequest();
                 ccc.updateSession(usr);
+                return null;
+            }
+
+            @Override
+            public Void visitEnrollmentMode() {
+                EnrollmentMode mode = EnrollmentMode.valueOf(value);
+                final EnrollmentSettings settings = session.getEnrollmentSettings();
+                final StandardEnrollmentSettings newSettings;
+                if(settings == null) {
+                    newSettings = new StandardEnrollmentSettings(null, null, null, mode);
+                } else {
+                    newSettings = new StandardEnrollmentSettings(settings.isEnabled(), settings.getFrom(), settings.getTo(), mode);
+                }
+                final UpdateSessionRequest usr = UpdateSessionRequestBuilder.newBuilder(caller, courseSessionId).setEnrollmentSettings(newSettings).createUpdateSessionRequest();
+                ccc.updateSession(usr);
+                return null;
+            }
+
+            @Override
+            public Void visitEnrollmentFromDate() {
+                return null;
+            }
+
+            @Override
+            public Void visitEnrollmentToDate() {
+                return null;
+            }
+
+            @Override
+            public Void visitDisenrollmentMode() {
+                EnrollmentMode mode = EnrollmentMode.valueOf(value);
+                final DisenrollmentSettings settings = session.getDisenrollmentSettings();
+                final StandardDisenrollmentSettings newSettings;
+                if(settings == null) {
+//                    newSettings = new StandardDisenrollmentSettings(Instant.now(), Instant.now(), mode);
+                    newSettings = new StandardDisenrollmentSettings(null, null, mode);
+                } else {
+                    newSettings = new StandardDisenrollmentSettings(settings.getFrom(), settings.getTo(), mode);
+                }
+//                final ExtendedCatalogCourseSession extSession = getExtendedSession(ccc, courseSessionId);
+//                extSession.getExtendedDisenrollmentSettings(); ???
+
+                final UpdateSessionRequest usr = UpdateSessionRequestBuilder.newBuilder(caller, courseSessionId).setUnenrollmentSettings(newSettings).createUpdateSessionRequest();
+                ccc.updateSession(usr);
+                return null;
+            }
+
+            @Override
+            public Void visitDisenrollmentFromDate() {
+                return null;
+            }
+
+            @Override
+            public Void visitDisenrollmentToDate() {
+                return null;
+            }
+
+            @Override
+            public Void visitParticipationEnabled() {
+                return null;
+            }
+
+            @Override
+            public Void visitParticipationShowName() {
+                return null;
+            }
+
+            @Override
+            public Void visitParticipationShowThumbnail() {
                 return null;
             }
         });
@@ -71,8 +146,9 @@ public class ProjectSessionJsonModule extends AbstractJsonAuthModule {
         return jsonTarget(map);
     }
 
-
-
+    private ExtendedCatalogCourseSession getExtendedSession(CourseCatalogClient ccc, CatalogCourseSessionId courseSessionId) {
+        return CollectionsUtil.singleItemOrNull(ccc.listExtendedSessions(new ListCatalogSessionRequestBuilder().withId(courseSessionId).build()));
+    }
 
     protected void checkSessionPermission(RequestCycle cycle, CatalogCourseSession courseSession) { // TODO: implement
 
--- src/main/java/se/dabox/cocobox/cpweb/module/project/session/SessionField.java
+++ src/main/java/se/dabox/cocobox/cpweb/module/project/session/SessionField.java
@@ -14,6 +14,51 @@ public enum SessionField {
         public <E> E accept( SessionFieldVisitor<E> visitor ) {
             return visitor.visitVisibility();
         }
+    },
+    enrollmentMode {
+        public <E> E accept( SessionFieldVisitor<E> visitor ) {
+            return visitor.visitEnrollmentMode();
+        }
+    },
+    enrollmentFromDate {
+        public <E> E accept( SessionFieldVisitor<E> visitor ) {
+            return visitor.visitEnrollmentFromDate();
+        }
+    },
+    enrollmentToDate {
+        public <E> E accept( SessionFieldVisitor<E> visitor ) {
+            return visitor.visitEnrollmentToDate();
+        }
+    },
+    disenrollmentMode {
+        public <E> E accept( SessionFieldVisitor<E> visitor ) {
+            return visitor.visitDisenrollmentMode();
+        }
+    },
+    disenrollmentFromDate {
+        public <E> E accept( SessionFieldVisitor<E> visitor ) {
+            return visitor.visitDisenrollmentFromDate();
+        }
+    },
+    disenrollmentToDate {
+        public <E> E accept( SessionFieldVisitor<E> visitor ) {
+            return visitor.visitDisenrollmentToDate();
+        }
+    },
+    participationEnabled {
+        public <E> E accept( SessionFieldVisitor<E> visitor ) {
+            return visitor.visitParticipationEnabled();
+        }
+    },
+    participationShowName {
+        public <E> E accept( SessionFieldVisitor<E> visitor ) {
+            return visitor.visitParticipationShowName();
+        }
+    },
+    participationShowThumbnail {
+        public <E> E accept( SessionFieldVisitor<E> visitor ) {
+            return visitor.visitParticipationShowThumbnail();
+        }
     };
 
     public abstract <E> E accept( SessionFieldVisitor<E> visitor );
@@ -21,5 +66,14 @@ public enum SessionField {
     public interface SessionFieldVisitor<E> {
         E visitDescription();
         E visitVisibility();
+        E visitEnrollmentMode();
+        E visitEnrollmentFromDate();
+        E visitEnrollmentToDate();
+        E visitDisenrollmentMode();
+        E visitDisenrollmentFromDate();
+        E visitDisenrollmentToDate();
+        E visitParticipationEnabled();
+        E visitParticipationShowName();
+        E visitParticipationShowThumbnail();
     }
 }
