69abafa85bdd7f2a930494d76a706db8ae720fb6
Magnus Andersson <magnus.andersson@learnifier.com>
2016-10-21 13:59:15 +0200

    #1674, problem att ta bort användare ur grupper. Jag fixade en del andra problem, tex användes både orgId och cugId på flera ställen där det räcker med cugId. Lade även till mer säkerhetskontroller.

--- src/main/java/se/dabox/cocobox/cpweb/module/cug/ClientUserGroupJsonModule.java
+++ src/main/java/se/dabox/cocobox/cpweb/module/cug/ClientUserGroupJsonModule.java
@@ -5,8 +5,13 @@ package se.dabox.cocobox.cpweb.module.cug;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import com.google.common.collect.ImmutableMap;
 import net.unixdeveloper.druwa.RequestCycle;
 import net.unixdeveloper.druwa.RequestTarget;
 import net.unixdeveloper.druwa.annotation.WebAction;
@@ -16,15 +21,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import se.dabox.cocobox.cpweb.module.core.AbstractJsonAuthModule;
 import se.dabox.cocobox.cpweb.module.user.UserModule;
+import se.dabox.cocobox.security.permission.CocoboxPermissions;
 import se.dabox.cocobox.security.user.OrgRoleName;
+import se.dabox.cocobox.vfs.InvalidArgumentException;
 import se.dabox.cocosite.druwa.CocoSiteConstants;
 import se.dabox.cocosite.org.MiniOrgInfo;
 import se.dabox.cocosite.user.MiniUserAccountHelper;
+import se.dabox.cocosite.webmessage.WebMessage;
+import se.dabox.cocosite.webmessage.WebMessageType;
+import se.dabox.cocosite.webmessage.WebMessages;
 import se.dabox.service.client.CacheClients;
 import se.dabox.service.cug.client.ClientUserGroup;
 import se.dabox.service.cug.client.ClientUserGroupClient;
 import se.dabox.service.login.client.UserAccount;
 import se.dabox.service.webutils.json.JsonEncoding;
+import se.dabox.service.webutils.login.LoginUserAccountHelper;
+
+import static com.segment.analytics.messages.Message.Type.group;
 
 /**
  *
@@ -38,7 +51,8 @@ public class ClientUserGroupJsonModule extends AbstractJsonAuthModule {
     @WebAction
     public RequestTarget onListClientUserGroups(RequestCycle cycle, String strOrgId) {
         MiniOrgInfo org = secureGetMiniOrg(cycle, strOrgId);
-        
+        checkOrgPermission(cycle, org.getId(), CocoboxPermissions.CP_VIEW_USER);
+
         List<ClientUserGroup> cugs = getClientUserGroupService(cycle).listGroups(org.getId())
                 .stream().filter(
                         m -> m.getParent() == null
@@ -48,24 +62,102 @@ public class ClientUserGroupJsonModule extends AbstractJsonAuthModule {
     }
     
     @WebAction
-    public RequestTarget onListClientUserGroupChildren(RequestCycle cycle, String strOrgId, String strCugId) {
-        MiniOrgInfo org = secureGetMiniOrg(cycle, strOrgId);
-        
-        List<ClientUserGroup> cugs = getClientUserGroupService(cycle).listChildren(Long.parseLong(strCugId));
+    public RequestTarget onListClientUserGroupChildren(RequestCycle cycle, String strCugId) {
+        ClientUserGroupClient cugService = getClientUserGroupService(cycle);
+        final Long groupId;
+        try {
+            groupId = Long.valueOf(strCugId);
+        } catch(NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid group id");
+        }
+
+        final ClientUserGroup group = cugService.getGroup(groupId);
+        if(group == null) {
+            throw new IllegalArgumentException("Can not find group" + strCugId);
+        }
+
+        final MiniOrgInfo org = secureGetMiniOrg(cycle, group.getOrgId());
+        checkOrgPermission(cycle, org.getId(), CocoboxPermissions.CP_VIEW_USER);
+
+        List<ClientUserGroup> cugs = cugService.listChildren(Long.parseLong(strCugId));
         
         return jsonTarget(toJsonUserClientUserGroups(cycle, cugs));
     }
     
     @WebAction
-    public RequestTarget onListClientUserGroupMembers(RequestCycle cycle, String strOrgId, String strCugId) {
-        MiniOrgInfo org = secureGetMiniOrg(cycle, strOrgId);
-        
+    public RequestTarget onListClientUserGroupMembers(RequestCycle cycle, String strCugId) {
         ClientUserGroupClient cugService = getClientUserGroupService(cycle);
+        final Long groupId;
+        try {
+            groupId = Long.valueOf(strCugId);
+        } catch(NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid group id");
+        }
+
+        final ClientUserGroup group = cugService.getGroup(groupId);
+        if(group == null) {
+            throw new IllegalArgumentException("Can not find group" + strCugId);
+        }
+
+        final MiniOrgInfo org = secureGetMiniOrg(cycle, group.getOrgId());
+        checkOrgPermission(cycle, org.getId(), CocoboxPermissions.CP_VIEW_USER);
+
+
         List<UserAccount> members = cugService.listGroupMembers(Long.valueOf(strCugId));
         
         return jsonTarget(toJsonUserAccounts(cycle, members, org.getId()));
     }
-    
+
+
+    @WebAction
+    public RequestTarget onRemoveClientUserGroupMembers(RequestCycle cycle, String strCugId) {
+        long caller = LoginUserAccountHelper.getCurrentCaller(cycle);
+        final String[] strIds = cycle.getRequest().getParameterValues("id[]");
+
+        final Long groupId;
+        try {
+            groupId = Long.valueOf(strCugId);
+        } catch(NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid group id");
+        }
+
+        ClientUserGroupClient cugService = getClientUserGroupService(cycle);
+        final ClientUserGroup group = cugService.getGroup(groupId);
+        if(group == null) {
+            throw new IllegalArgumentException("Can not find group" + strCugId);
+        }
+
+        final MiniOrgInfo org = secureGetMiniOrg(cycle, group.getOrgId());
+        checkOrgPermission(cycle, org.getId(), CocoboxPermissions.CP_EDIT_USER);
+
+        List<String> succeeded = new ArrayList<>();
+        List<String> failed = new ArrayList<>();
+        if(strIds != null && strIds.length != 0) {
+            for(String strId: strIds) {
+                try {
+                    Long id = Long.valueOf(strId);
+                    cugService.removeGroupMember(caller, groupId, id);
+                    succeeded.add(strId);
+                } catch(NumberFormatException e) {
+                    failed.add(strId);
+                }
+            }
+        }
+        WebMessages webMessages = WebMessages.getInstance(cycle);
+        if(succeeded.size() > 0) {
+            webMessages.addMessage(WebMessage.createTextMessage("Removed " + succeeded.size() + (succeeded.size()==1?" user":" users") + " from group.", WebMessageType.info));
+        }
+        if(failed.size() > 0) {
+            webMessages.addMessage(WebMessage.createTextMessage("Failed to remove " + failed.size() + (failed.size()==1?" user":" users") + " from group.", WebMessageType.error));
+        }
+
+        return jsonTarget(ImmutableMap.of(
+                "status", "ok",
+                "removed", succeeded,
+                "failed", failed
+        ));
+    }
+
     private ClientUserGroupClient getClientUserGroupService(RequestCycle cycle) {
         return CacheClients.getClient(cycle, ClientUserGroupClient.class);
     }
--- src/main/webapp/js/cug/cugMembersOverview.js
+++ src/main/webapp/js/cug/cugMembersOverview.js
@@ -1,80 +1,148 @@
 /* 
  * (c) Dabox AB 2015 All Rights Reserved
  */
-define(['cocobox-icheck'], function() {
+define(['cocobox-icheck', 'es6-shim', 'messenger'], function() {
     "use strict";
-    var exports = {};
+    var exports = {}, settings;
 
-    
+
     function runiCheck(cell) {
-      $('input', cell).icheck({
-        checkboxClass: 'icheckbox_square-red',
-        radioClass: 'iradio_square-red'
-      });
-    };
+        $('input', cell).icheck({
+            checkboxClass: 'icheckbox_square-red',
+            radioClass: 'iradio_square-red'
+        });
+    }
 
-   $(document).ready(function() {
-        require(['dataTables-bootstrap'], function() {
-        var oTable = $('#listcugs').dataTable({
-            "dom": '<"row"<"col-sm-6"f><"col-sm-6">><"row"<"col-sm-12"rt>><"row"<"col-sm-6"i><"col-sm-6"p>>',
-            "order": [[0,'asc']],
-            "initComplete": function() {
-                $('#listcugs_filter input').attr('placeholder', 'Search  Groups');
-            },
-            "columnDefs": [ 
-                {
-                    "targets": [0],
-                    "data": "uid",
-                    "orderable": false,
-                    "width": "24px",
-                    "render": function(data) {
-                        return  '<input type="checkbox" class="rowcb" onchange="return cpweb.rowcbChange(this)" data-rowid="' + data + '"/>';
-                    },
-                    "createdCell": function (td, cellData, rowData, row, col) {
-                        runiCheck(td);
-                    }
-                },
-                {
-                    "targets": [ 1 ],
-                    "className": "block-link",
-                    "data" : function(row, type, set) {
-                        console.log("---", row, type, set);
-                        if (!row.nameDisplay) {
-                            var name = (row.name && row.name.length > 0) ? row.name : "(Name not set yet)";
-                            row.nameDisplay = '<a href="' + row.link + '">' +  name +'</a> ';
-                        }
+    function rowcbChange(checkbox) {
+        var pForm = $(checkbox).parents('form').first();
 
-                        if (type === 'display') {
-                            return row.nameDisplay;
-                        } else if (type === 'filter') {
-                            return row.name;
-                        } else if (type === 'sort') {
-                            return row.name;
-                        } else {
-                            //Anything else and raw row
-                            return row.name;
-                        }
-                    }
-                },
-                {
-                    "targets": [ 2 ],
-                    "data": "email"
-                }
-            ],
-            "ajax": listMembersAjaxSource,
-            "pageLength": 25,
-            "pagingType": "full_numbers",
-            "deferRender": true,
-            "language": {
-                "search": "",
-                "zeroRecords": "No group matches your query",
-                "emptyTable": "<span class='emptytable'>No groups have been added.</a></span>",
-                "loadingRecords": "<p>Loading groups...</p><img src='" + spinnerUrl + "' />"
+        if (pForm == null) {
+            log('No form found');
+            return false;
+        }
+
+        var hiddenString = $(pForm).find("input[name=__ids]").val();
+
+        var val = String($(checkbox).data('rowid'));
+
+        var allValues = hiddenString == "" ? [] : hiddenString.split(',');
+
+        var checked = $(checkbox).prop('checked');
+
+        if (checked) {
+            if (jQuery.inArray(val, allValues) == -1) {
+                allValues.push(val);
             }
-        });
-        });
-                
-    } );
+        } else {
+            //Remove state
+            var index = jQuery.inArray(val, allValues);
+            if (index != -1) {
+                allValues.splice(index,1);
+            }
+        }
 
+        hiddenString = allValues.join(',');
+
+        $(pForm).find("input[name=__ids]").val(hiddenString);
+
+        console.log('Setting selected values to ',hiddenString);
+
+        return true;
+    }
+
+    exports.init = function(options) {
+        settings = $.extend({
+            removeMemberUrl: undefined
+        }, options || {});
+
+        $(document).ready(function() {
+            require(['dataTables-bootstrap'], function() {
+                var oTable = $('#listcugs').dataTable({
+                    "dom": '<"row"<"col-sm-6"f><"col-sm-6">><"row"<"col-sm-12"rt>><"row"<"col-sm-6"i><"col-sm-6"p>>',
+                    "order": [[0,'asc']],
+                    "initComplete": function() {
+                        $('#listcugs_filter input').attr('placeholder', 'Search  Groups');
+                    },
+                    "columnDefs": [
+                        {
+                            "targets": [0],
+                            "data": "uid",
+                            "orderable": false,
+                            "width": "24px",
+                            "render": function(data) {
+                                return  '<input type="checkbox" class="rowcb" data-rowid="' + data + '"/>';
+                            },
+                            "createdCell": function (td, cellData, rowData, row, col) {
+                                runiCheck(td);
+                            }
+                        },
+                        {
+                            "targets": [ 1 ],
+                            "className": "block-link",
+                            "data" : function(row, type, set) {
+                                if (!row.nameDisplay) {
+                                    var name = (row.name && row.name.length > 0) ? row.name : "(Name not set yet)";
+                                    row.nameDisplay = '<a href="' + row.link + '">' +  name +'</a> ';
+                                }
+
+                                if (type === 'display') {
+                                    return row.nameDisplay;
+                                } else if (type === 'filter') {
+                                    return row.name;
+                                } else if (type === 'sort') {
+                                    return row.name;
+                                } else {
+                                    //Anything else and raw row
+                                    return row.name;
+                                }
+                            }
+                        },
+                        {
+                            "targets": [ 2 ],
+                            "data": "email"
+                        }
+                    ],
+                    "ajax": listMembersAjaxSource,
+                    "pageLength": 25,
+                    "pagingType": "full_numbers",
+                    "deferRender": true,
+                    "language": {
+                        "search": "",
+                        "zeroRecords": "No group matches your query",
+                        "emptyTable": "<span class='emptytable'>No memembers has been added.</a></span>",
+                        "loadingRecords": "<p>Loading groups...</p><img src='" + spinnerUrl + "' />"
+                    }
+                });
+            });
+            $(document).on('change', 'input.rowcb', function(){
+                rowcbChange(this);
+            });
+            // onclick="return cpweb.rosterDelete(this, $('#cugmembersform'), 'removeMembers', '${ctext("cpweb.cug.members.remove.title")?js_string}', '${ctext("cpweb.cug.members.remove.text")?js_string}');
+            $("#remove-member-button").click(function(){
+                var ids = $("input[name=__ids]").val();
+                if(ids) {
+                    var idArray = ids.split(",");
+                    if(idArray.length > 0) {
+                        require(['dabox-common'], function() {
+                            cocobox.confirmationDialog('Remove member from group',
+                                'Are you sure you want to remove ' + idArray.length + ' members from the group?',
+                                function() {
+                                    $.post(settings.removeMemberUrl, {id: idArray}).done(function(data){
+                                        if(data.status == "ok") {
+                                            window.location = window.location; // Error message will be generated from webaction for now
+                                        } else {
+                                            CCBMessengerError("Error when removing users: ", data.message);
+                                        }
+                                    }).fail(function(jqXHR, textStatus, errorThrown){
+                                        CCBMessengerError("Error when removing users: ", errorThrown);
+                                    });
+                                });
+                        });
+                    }
+                }
+                return false;
+            });
+        });
+    };
     return exports;
 });
