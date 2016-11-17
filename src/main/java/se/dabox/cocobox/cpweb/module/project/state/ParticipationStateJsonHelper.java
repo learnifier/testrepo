917d84e8b3a38722619e5297306552390a7689bb
Magnus Andersson <magnus.andersson@learnifier.com>
2016-11-17 16:28:25 +0100

    Javadoc + fixed a few left over non-generic types.

--- src/main/java/se/dabox/cocobox/cpweb/module/project/state/ParticipationStateJsonHelper.java
+++ src/main/java/se/dabox/cocobox/cpweb/module/project/state/ParticipationStateJsonHelper.java
@@ -6,7 +6,6 @@ import net.unixdeveloper.druwa.RequestCycle;
 import net.unixdeveloper.druwa.ServiceRequestCycle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import se.dabox.cocobox.cpweb.module.project.event.ParticipationEvent;
 import se.dabox.service.client.CacheClients;
 import se.dabox.service.common.ccbc.CocoboxCoordinatorClient;
 import se.dabox.service.common.ccbc.project.ProjectParticipationState;
@@ -23,7 +22,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
- * Helper to read/write participation state data in json format.
+ * Helper to read/write participation state data in json format. Type T is a class that can be serialized by Jackson.
  *
  * @author Magnus Andersson (magnus.andersson@learnifier.com)
  */
@@ -37,8 +36,12 @@ public class ParticipationStateJsonHelper<T> {
             LoggerFactory.getLogger(ParticipationStateJsonHelper.class);
 
     /**
+     * Create an instance that works with a specific cycle and prefix.
      *
-     * @param cycle
+     * The extra typeParameterClass parameter is needed to work around type erasure problem for object (de-)serialization.
+     *
+     * @param typeParameterClass Class of type.
+     * @param cycle Cycle.
      * @param prefix Prefix of state data. Should include the trailing dot (e.g. "event.").
      */
     public ParticipationStateJsonHelper(Class<T> typeParameterClass, RequestCycle cycle, String prefix) {
@@ -48,9 +51,10 @@ public class ParticipationStateJsonHelper<T> {
     }
 
     /**
+     * Return all state data with prefix for one participation.
      *
-     * @param participationId
-     * @return List of ParticipationsEvent .
+     * @param participationId Id of participation
+     * @return List of objects of type T
      */
     public @Nonnull List<T> getParticipationEvents(long participationId) {
         CocoboxCoordinatorClient ccbc = getCocoboxCordinatorClient(cycle);
@@ -68,11 +72,12 @@ public class ParticipationStateJsonHelper<T> {
     }
 
     /**
+     * Return all state data with prefix for multiple participations.
      *
      * @param participationIds
-     * @return
+     * @return Map of participation id -> List of objects of type T
      */
-    public Map<Long, List<T>> getParticipationEvents(List<Long> participationIds) {
+    public Map<Long, List<T>> getParticipationEvents(@Nonnull List<Long> participationIds) {
         CocoboxCoordinatorClient ccbc = getCocoboxCordinatorClient(cycle);
         final List<ProjectParticipationState> states = ccbc.getParticipationState(participationIds);
 
@@ -96,7 +101,14 @@ public class ParticipationStateJsonHelper<T> {
         return Collections.emptyMap();
     }
 
-    public @Nullable T getParticipationEvent(long participationId, String cid) {
+    /**
+     * Lookup first matching participation state with matching prefix + id.
+     *
+     * @param participationId
+     * @param id
+     * @return Object of type T created from json data.
+     */
+    public @Nullable T getParticipationState(long participationId, String id) {
         CocoboxCoordinatorClient ccbc = getCocoboxCordinatorClient(cycle);
         ProjectParticipationState state = ccbc.getParticipationState(participationId);
 
@@ -105,7 +117,7 @@ public class ParticipationStateJsonHelper<T> {
 
             if (stateMap != null) {
                 return stateMap.entrySet().stream()
-                        .filter(e -> e.getKey() != null && e.getKey().equals(prefix + cid))
+                        .filter(e -> e.getKey() != null && e.getKey().equals(prefix + id))
                         .map(e -> fromJson(e.getValue()))
                         .flatMap(o -> o.isPresent() ? Stream.of(o.get()) : Stream.empty())  // In Java 9: .flatMap(Optional::stream)
                         .findFirst()
@@ -115,25 +127,32 @@ public class ParticipationStateJsonHelper<T> {
         return null;
     }
 
+    /**
+     * Sets participation state data.
+     *
+     * @param participationId Id of participation.
+     * @param id Id to save under. "prefix" will prepended to id for the actual key saved under.
+     * @param data Object that will be serialized to json.
+     */
+    public void setParticipationEvent(long participationId, String id, @Nullable T data) {
+        final CocoboxCoordinatorClient ccbc = getCocoboxCordinatorClient(cycle);
+
+        String field = prefix + id;
+
+        try {
+            String st = MAPPER.writeValueAsString(data);
+            ccbc.setParticipationState(participationId, field, st);
+        } catch(JsonProcessingException e) {
+            throw new IllegalArgumentException("Could not serialize object");
+        }
+    }
+
     private Optional<T> fromJson(String jsonString) {
         try {
             final T obj = MAPPER.readValue(jsonString, typeParameterClass);
             return Optional.of(obj);
         } catch(IOException e) {
             return Optional.empty();
-        }
-    }
-
-    public void setParticipationEvent(String id, ParticipationEvent event, long participationId) {
-        final CocoboxCoordinatorClient ccbc = getCocoboxCordinatorClient(cycle);
-
-        String field = prefix + id;
-
-        try {
-            String st = MAPPER.writeValueAsString(event);
-            ccbc.setParticipationState(participationId, field, st);
-        } catch(JsonProcessingException e) {
-            throw new IllegalArgumentException("Could not serialize object");
         }
     }
 
